0a360a667a3c920196a0d45cdc14235aafc15304
Arvind Mukund <MasterAwesome@users.noreply.github.com>
2015-12-02 11:58:05 +0530

    Merge BoardConfig.mk
    Signed-off-by: Arvind Mukund <MasterAwesome@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,14 +45,11 @@ TARGET_CPU_VARIANT := krait
 TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Kernel
-# BOARD_CUSTOM_BOOTIMG_MK := $(PLATFORM_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=onyx user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-# BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 01000000 --tags_offset 00000100 --dt device/oneplus/onyx/dt.img
 TARGET_KERNEL_ARCH := arm
-# TARGET_KERNEL_CONFIG := cyanogenmod_onyx_defconfig
 TARGET_KERNEL_CONFIG := opx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 
