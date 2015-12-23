2ee04444a5f69ee6867f6d4b547d8db8f09248a6
Arvind Mukund <armu30@gmail.com>
2015-12-24 00:49:55 +0530

    lights: Remove button backlight feature
    Our device doesn't support button brightness. Return -EINVAL
    
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -52,9 +52,6 @@ char const*const BLUE_LED_FILE
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
 
-char const*const BUTTON_FILE
-        = "/sys/class/leds/button-backlight/brightness";
-
 char const*const RED_BLINK_FILE
         = "/sys/class/leds/red/blink";
 
@@ -214,17 +211,6 @@ set_light_attention(struct light_device_t* dev,
 }
 
 static int
-set_light_buttons(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    int err = 0;
-    pthread_mutex_lock(&g_lock);
-    err = write_int(BUTTON_FILE, state->color & 0xFF);
-    pthread_mutex_unlock(&g_lock);
-    return err;
-}
-
-static int
 set_light_battery(struct light_device_t *dev,
         const struct light_state_t *state)
 {
@@ -266,8 +252,6 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_backlight;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else if (0 == strcmp(LIGHT_ID_BATTERY, name))
