48c7a89768c9a48c7fd5a6b54b566090a260d672
Arvind Mukund <armu30@gmail.com>
2016-01-02 21:29:17 +0530

    update copyright headers
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- init/init_onyx.c
+++ init/init_onyx.c
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2015, The CyanogenMod Project
+   Copyright (c) 2016, Arvind Mukund <armu30@gmail.com>
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
--- liblight/lights.c
+++ liblight/lights.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project.
  * Copyright (C) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 Arvind Mukund <armu30@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
