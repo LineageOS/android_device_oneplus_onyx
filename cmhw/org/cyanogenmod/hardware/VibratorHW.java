a38d88ee73391becefab54589712e8fe0c616b94
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 21:27:43 +0300

    cmhw: Use cmsdk FileUtils
    Change-Id: Idebbc357888c8670829a70c69c2fa99f717b45c6

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,16 +16,14 @@
 
 package org.cyanogenmod.hardware;
 
-import java.io.File;
-
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 public class VibratorHW {
 
     private static String AMP_PATH = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
 
     public static boolean isSupported() {
-        return new File(AMP_PATH).exists();
+        return FileUtils.isFileWritable(AMP_PATH);
     }
 
     public static int getMaxIntensity()  {
