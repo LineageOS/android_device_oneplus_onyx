da817c9ca87d8f89236054e4b0188cde67dc1cea
PSSGCSim <pssgcsim@gmail.com>
2017-01-02 19:16:57 +0100

    onyx: Rebrand to LineageOS
    Change-Id: I1b0a329a328d7eb2c19903e3b545bb6171965e07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := cm_onyx_defconfig
+TARGET_KERNEL_CONFIG := lineage_onyx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
 
 # ANT+
