922cd684e5b2c679dbc440192b4c6d8e468bda3b
oelf <oelfyo@gmail.com>
2017-01-11 11:19:23 +0100

    makefiles: update for LineageOS
    Change-Id: I031bfc4e2595de9d0580ceaa6308e5450b6cad61
    Signed-off-by: oelf <oelfyo@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := cm_onyx
+PRODUCT_NAME := lineage_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
