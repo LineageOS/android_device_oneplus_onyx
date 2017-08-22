8a01c4c7e1480f1bd02db7baafe427857c59b47a
Simon Shields <simon@lineageos.org>
2017-08-23 00:32:04 +1000

    onyx: s@vendor/cm@vendor/lineage@g
    Change-Id: Ib4553f64ca461fd395675b53b526716a528cac69

--- lineage.mk
+++ lineage.mk
@@ -18,8 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
